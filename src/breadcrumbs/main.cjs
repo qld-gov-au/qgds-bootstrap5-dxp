38084b9e86a8dc103b4f1eaa29c6682f44eef7bb
Sen Chung <SenChung@users.noreply.github.com>
2024-09-11 15:08:21 +1000

    Cleaner dev structure

--- src/breadcrumbs/main.cjs
+++ src/breadcrumbs/main.cjs
@@ -3,60 +3,31 @@
  * Common js module to support DXP Component Service.
  */
 module.exports = async function (input, info) {
-  const getBreadcrumbData = function () {
-    let breadcrumbHtml = "";
-    const navData = [
-      {
-        link: "#",
-        linktext: "Home",
-      },
-      {
-        link: "#",
-        linktext: "Topic 01",
-      },
-      {
-        link: "#",
-        linktext: "Topic 02",
-      },
-      {
-        link: "#",
-        linktext: "Topic 03",
-      },
-      {
-        link: "#",
-        linktext: "Current page",
-      },
-    ];
-    const breadcrumbLength = Number(navData.length);
-
-    for (const navItems in navData) {
-      if (Number(navItems) + 1 < breadcrumbLength) {
-        breadcrumbHtml += `<li class="breadcrumb-item">
-                            <a href="${navData[navItems].link}">
-                              ${navData[navItems].linktext}                      
-                            </a>
-                          </li>`;
-      } else {
-        breadcrumbHtml += `<li class="breadcrumb-item active" aria-current="page" data-href="${navData[navItems].link}">                  
-                            ${navData[navItems].linktext}
-                          </li>`;
-      }
-    }
-
-    return breadcrumbHtml;
-  };
+  const itemLength = input.breadcrumbs.length;
+  const items = input.breadcrumbs
+    .map(
+      (item, i) => `
+      <li class="breadcrumb-item">
+        ${i + 1 < itemLength ? `<a href="${item.link}">` : ""}
+          ${item.linktext}
+        ${i + 1 < itemLength ? `</a>` : ""}
+      </li>
+    `
+    )
+    .join("");
   return `
-          <div class="container-fluid ${input.colour}">
-            <div class="container">
-              <div class="row">
-                <div class="container-xl">
-                  <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                      ${getBreadcrumbData()}
-                    </ol>
-                  </nav>
-                </div>
-              </div>
+      <div class="container-fluid ${input.brandOverride ? input.brandOverride : ""}">
+        <div class="container">
+          <div class="row">
+            <div class="container-xl">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  ${items}
+                </ol>
+              </nav>
             </div>
-          </div>`;
+          </div>
+        </div>
+      </div>
+    `;
 };
